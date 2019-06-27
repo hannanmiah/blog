0f797b4393e25690bb422119f5f49eb63a11ddc3
Hannan Miah <hannanhridoy@gmail.com>
2019-06-28 01:55:14 +0600

    just testing some code test.php

--- test/test.php
+++ test/test.php
@@ -1,7 +1,14 @@
 <?php
-for ($i = 0; $i < 5; $i++) {
-    for ($j = 5; $j > $i; $j--) {
-        echo "* ";
-    }
-    echo "\n";
+
+$mystring = 'abc';
+$findme   = 'd';
+$pos = strpos($mystring, $findme);
+
+// Note our use of ===.  Simply == would not work as expected
+// because the position of 'a' was the 0th (first) character.
+if ($pos == false) {
+    echo "The string '$findme' was not found in the string '$mystring'";
+} else {
+    echo "The string '$findme' was found in the string '$mystring'";
+    echo " and exists at position $pos";
 }
