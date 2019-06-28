7e7b85d75deb71024091d2d4bd272fc516f826f7
Hannan Miah <hannanhridoy@gmail.com>
2019-06-28 16:46:03 +0600

    Just tested some code at test.php

--- test/test.php
+++ test/test.php
@@ -1,14 +1,16 @@
 <?php
-
-$mystring = 'abc';
-$findme   = 'd';
-$pos = strpos($mystring, $findme);
-
-// Note our use of ===.  Simply == would not work as expected
-// because the position of 'a' was the 0th (first) character.
-if ($pos == false) {
-    echo "The string '$findme' was not found in the string '$mystring'";
-} else {
-    echo "The string '$findme' was found in the string '$mystring'";
-    echo " and exists at position $pos";
+class Test
+{
+    public $test = ['name' => 'Hannan', 'age' => 22, 'class' => 'Engineering'];
+    public function __unset($name)
+    {
+        if (array_key_exists($name, $this->test)) {
+            unset($this->test[$name]);
+        }
+    }
 }
+
+$t = new Test();
+unset($t->age);
+unset($t->class);
+var_dump($t);
